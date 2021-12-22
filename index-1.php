3ef3b3e69a4a4fdf9564b39aa3dd621293122394
Md.Mahadi Hasan <64768414+mahadiweb@users.noreply.github.com>
2021-12-22 22:06:35 +0600

    Create index-1.php

---
+++ index-1.php
@@ -0,0 +1,53 @@
+<?php
+$conn = mysqli_connect("localhost","root","","user");
+
+
+if (isset($_POST['setpermission'])) {
+	//print_r($_POST);
+	$setpermission = serialize($_POST['setpermission']);
+
+	//$query = mysqli_query($conn, "INSERT INTO post(name,pass,status) VALUES('test','0','1')");
+	$query = mysqli_query($conn, "UPDATE user SET permission='$setpermission' WHERE id=2");
+}
+	$query = mysqli_query($conn, "SELECT * FROM user WHERE id=2");
+	$res = mysqli_fetch_assoc($query);
+	$permission = $res['permission'];
+	$per = unserialize($permission);
+	//print_r($per);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Permission</title>
+</head>
+<body>
+	<form action="" method="post">
+		<input type="checkbox" name="setpermission[]" <?php if (in_array("post", $per)){echo "checked";} ?> value="post">Post
+		<input type="checkbox" name="setpermission[]" value="edit" <?php if (in_array("edit", $per)){echo "checked";} ?>>Edit
+		<input type="checkbox" name="setpermission[]" value="delete" <?php if (in_array("delete", $per)){echo "checked";} ?>>Delete
+		<input type="checkbox" name="setpermission[]" value="email" <?php if (in_array("email", $per)){echo "checked";} ?>>Email
+		<input type="checkbox" name="setpermission[]" value="settings" <?php if (in_array("settings", $per)){echo "checked";} ?>>Settings
+		<input type="submit">
+	</form>
+
+<?php
+if (in_array("post", $per)) {
+	echo '<a href="#">post</a><br>';
+}
+if (in_array("edit", $per)) {
+	echo '<a href="#">edit</a><br>';
+}
+if (in_array("delete", $per)) {
+	echo '<a href="#">delete</a><br>';
+}
+if (in_array("email", $per)) {
+	echo '<a href="#">email</a><br>';
+}
+if (in_array("settings", $per)) {
+	echo '<a href="#">settings</a>';
+}
+?>
+</body>
+</html>
